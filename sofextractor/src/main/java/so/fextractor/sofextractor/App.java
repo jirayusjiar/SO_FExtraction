d6d31021900bfeb95c78bb0f95a30af54ee2246d
jirayusjiar <jiyoyo@hotmail.com>
2016-04-01 19:16:31 +0900

    Init mvn project and gitignore

---
+++ sofextractor/src/main/java/so/fextractor/sofextractor/App.java
@@ -0,0 +1,13 @@
+package so.fextractor.sofextractor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
